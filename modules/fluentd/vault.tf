3c75214701e5682869e9406063ce85fc2d121a1b
Curtis Tan <merwinwee@gmail.com>
2019-10-09 17:35:54 +0800

    Tentatively use federation_token for fluentd

--- modules/fluentd/vault.tf
+++ modules/fluentd/vault.tf
@@ -5,8 +5,9 @@ locals {
 resource "vault_aws_secret_backend_role" "logs" {
   count = var.logs_s3_enabled ? 1 : 0
 
-  name    = var.log_vault_role
-  backend = var.vault_sts_path
+  name            = var.log_vault_role
+  backend         = var.vault_sts_path
+  credential_type = "federation_token"
 
   policy_arn = aws_iam_policy.logs_s3.*.arn[0]
 }
